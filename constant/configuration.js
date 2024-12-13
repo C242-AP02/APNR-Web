7af9a7ba7bbb065ac5eb9aaaa5e99d133df752d2
aorus22 <alyzanadif04@gmail.com>
2024-12-13 15:10:08 +0700

    fix

--- constant/configuration.js
+++ constant/configuration.js
@@ -1,4 +1,2 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-
-// export const BACKEND_URL = "https://apnr-backend-580245180821.asia-southeast2.run.app"
+export const BACKEND_URL = "https://apnr-backend-580245180821.asia-southeast2.run.app"
 // export const BACKEND_URL = "http://localhost:9000"
